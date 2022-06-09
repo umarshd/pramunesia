d1222659c5a45ee907b3d58585093840ec222da4
virgiliusnanamanek <84966287+virgiliusnanamanek@users.noreply.github.com>
2022-06-09 19:17:48 +0700

    membuat carousel

---
+++ components/afterlogin/carousel/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Fade } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
+
+const fadeImages = [
+  'https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+  'https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80',
+  'https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+];
+
+export default function Carousel() {
+  return (
+    <div className="slide-container">
+      <Fade>
+        <div className="each-fade">
+          <img src={fadeImages[0]} alt="" />
+        </div>
+        <div className="each-fade">
+          <img src={fadeImages[1]} alt="" />
+        </div>
+        <div className="each-fade">
+          <img src={fadeImages[2]} alt="" />
+        </div>
+      </Fade>
+    </div>
+  );
+}
