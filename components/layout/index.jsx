6ac20e6f72ae3f803ec5b82cb24ade30dab958f5
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2022-06-09 23:51:57 +0700

    bug fix layouting

--- components/layout/index.jsx
+++ components/layout/index.jsx
@@ -1,21 +1,17 @@
-import Head from 'next/head';
-import React from 'react';
-import Footer from '../footer';
-import Header from '../header';
+import Head from "next/head";
+import React from "react";
+import Footer from "../footer";
+import Header from "../header";
 
 function Layout({ children, pageTitle }) {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>
-          Pramunesia |
-          {' '}
-          {pageTitle}
-        </title>
+        <title>Pramunesia | {pageTitle}</title>
       </Head>
 
-      <div className="container d-flex flex-column">
+      <div className="container mx-auto">
         <Header />
         {children}
         <Footer />
