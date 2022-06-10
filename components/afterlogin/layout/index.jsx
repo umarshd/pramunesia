9086ffcfd330f16f31f2c8c89f9ed4451b4fe5a0
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2022-06-10 10:15:24 +0700

    bug fix layout

--- components/afterlogin/layout/index.jsx
+++ components/afterlogin/layout/index.jsx
@@ -1,21 +1,17 @@
-import Head from 'next/head';
-import React from 'react';
-import Footer from '../../footer';
-import Navigation from '../navbar';
+import Head from "next/head";
+import React from "react";
+import Footer from "../../footer";
+import Navigation from "../navbar";
 
 function Layout({ children, pageTitle }) {
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>
-          Pramunesia |
-          {' '}
-          {pageTitle}
-        </title>
+        <title>Pramunesia | {pageTitle}</title>
       </Head>
 
-      <div className="container">
+      <div className="container mx-auto">
         <Navigation />
         {children}
         <Footer />
