a6c7215d7e00d1fca6e4f5ec38df64033fc2047a
virgiliusnanamanek <84966287+virgiliusnanamanek@users.noreply.github.com>
2022-06-11 21:07:41 +0700

    membuat halaman user-search-guide

---
+++ pages/search-guide-user/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import GuideList from '../../components/afterlogin/guide-list-card';
+import Navigation from '../../components/afterlogin/navbar';
+
+function SearchUserGuide({ dataGuide }) {
+  return (
+    <div className="container mx-auto">
+      <Navigation />
+      <h2 className="text-center mb-4">Daftar Pemandu Wisata  Cirebon</h2>
+      <div className="row">
+        {dataGuide.map((guide) => (
+          <div className="col-lg-6 col-md-6 col-sm-12" key={guide.id}>
+            <GuideList
+              namaPemandu={guide.name}
+              rating={guide.id}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const dataGuide = await res.json();
+  return {
+    props: {
+      dataGuide,
+    },
+  };
+}
+
+export default SearchUserGuide;
