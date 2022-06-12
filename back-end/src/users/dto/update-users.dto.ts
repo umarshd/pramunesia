6e9a10c0813b05156a2968ac6f78dc4eb8c1b95a
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2022-06-12 23:37:32 +0700

    Update update-users.dto.ts
    penambahan hint untuk api

--- back-end/src/users/dto/update-users.dto.ts
+++ back-end/src/users/dto/update-users.dto.ts
@@ -1,19 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class UpdateUsersDto {
   @IsNotEmpty()
+  @ApiProperty()
   name: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   @IsEmail()
   email: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   address: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   telp: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   password: string;
 }
