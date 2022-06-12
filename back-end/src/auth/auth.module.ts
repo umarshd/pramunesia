fc3643b0b49160d818047bd6876c874c43846fff
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2022-06-12 23:39:10 +0700

    Create auth.module.ts

---
+++ back-end/src/auth/auth.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UsersModule } from 'src/users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+
+@Module({
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot(),
+    JwtModule.register({
+      secret: process.env.JWT_KEY,
+      signOptions: {
+        expiresIn: '12h',
+      },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
+  exports: [AuthService, JwtModule],
+})
+export class AuthModule {}
