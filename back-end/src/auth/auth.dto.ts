c061766ea2fbbf81353ff001df6f1955ea6f9f1f
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2022-06-13 22:37:01 +0700

    Update auth.dto.ts

--- back-end/src/auth/auth.dto.ts
+++ back-end/src/auth/auth.dto.ts
@@ -1,8 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class AuthDto {
   @IsNotEmpty()
+  @IsEmail()
   @ApiProperty()
   email: string;
 
