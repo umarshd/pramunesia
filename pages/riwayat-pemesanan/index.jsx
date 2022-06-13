94911836000992f7d4d182ef579ffce3211062ff
virgiliusnanamanek <84966287+virgiliusnanamanek@users.noreply.github.com>
2022-06-13 19:12:55 +0700

    create draft halaman riwayat-pemesanan

---
+++ pages/riwayat-pemesanan/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from '../../components/afterlogin/navbar';
+
+export default function BookingHistory() {
+  return (
+    <div className="container mx-auto">
+      <Navigation />
+      <h3>Pesanan Saya</h3>
+
+    </div>
+  );
+}
