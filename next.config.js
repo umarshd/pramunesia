043cb0522d6fd9269e2e8dd1216fbf36c5a98b8a
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2022-06-16 02:34:01 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   images: {
-    domains: ['pixabay.com', 'www.pexels.com', 'images.pexels.com', 'cdn.pixabay.com'],
+    domains: [
+      "pixabay.com",
+      "www.pexels.com",
+      "images.pexels.com",
+      "cdn.pixabay.com",
+    ],
   },
 };
