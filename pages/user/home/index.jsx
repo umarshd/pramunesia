c9b9679be81b27ca9621d475d9fdd27cb819e1e1
virgiliusnanamanek <vickynana034@gmail.com>
2022-06-15 21:24:02 +0700

    update user home page
    Memindahkan folder user-home-pages ke dalam folder user, kemudian mengganti nama foldernya menjadi home

---
+++ pages/user/home/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FiUsers } from 'react-icons/fi';
+import Carousel from '../../../components/afterlogin/carousel';
+import Card from '../../../components/afterlogin/home-card';
+import Layout from '../../../components/afterlogin/layout';
+import sessionUserLogin from '../../auth/sessionUserLogin';
+
+function UserHomePage() {
+  sessionUserLogin();
+  return (
+    <Layout pageTitle="user-home">
+      <Carousel />
+      <div className="my-5">
+        <h3 className="h3 my-3">Pilih sesuai kebutuhanmu</h3>
+        <div className="row my-3">
+          <div className="col-sm-12 col-md-6 col-lg-6">
+            <Card
+              icons={<FiUsers size="4rem" color="white" />}
+              title="Cari Pemandu Wisata"
+              desc="Lihat transaksi pembayaran
+          disini!"
+            />
+          </div>
+          <div className="col-sm-12 col-md-6 col-lg-6" />
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export default UserHomePage;
