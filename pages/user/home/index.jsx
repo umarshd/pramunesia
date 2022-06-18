03d648bdf21bd3dc73a9f72ca8068829637a51e3
Diandara TResya Haniva <diandaraa@gmail.com>
2022-06-19 00:32:25 +0700

    update user homepage

--- pages/user/home/index.jsx
+++ pages/user/home/index.jsx
@@ -10,11 +10,12 @@ function UserHomePage() {
   sessionUserLogin();
   return (
     <Layout pageTitle="user-home">
+      <h3 className="h3 my-3 mx-4">Rekomendasi Wisata di </h3>
       <Carousel />
       <div className="my-5">
-        <h3 className="h3 my-3">Pilih sesuai kebutuhanmu</h3>
+        <h3 className="h3 my-3 mx-4">Pilih sesuai kebutuhanmu</h3>
         <div className="row my-3">
-          <div className="col-sm-12 col-md-6 col-lg-6">
+          <div className="col-sm-12 col-md-6 col-lg-6 px-4">
             <Link href="user/search-guide">
               <Card
                 icons={<FiUsers size="4rem" color="white" />}
