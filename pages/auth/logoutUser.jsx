702aeb0c000a4f492ed9c7bcb420a070bb14a10a
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2022-06-25 18:55:50 +0700

    Update logoutUser.jsx

--- pages/auth/logoutUser.jsx
+++ pages/auth/logoutUser.jsx
@@ -3,6 +3,7 @@ import Router from "next/router";
 
 const logoutUser = async () => {
   await Cookies.remove("pramunesiaAppToken");
+  await Cookies.remove("pramunesiaAppTokenAdmin");
   await Cookies.remove("pramunesiaIdUser");
   await Cookies.remove("pramunesiaRole");
   await Cookies.remove("idCity");
