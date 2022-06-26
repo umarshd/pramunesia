9497f58c7595310c6fb6066798002ebb095541cf
Diandara TResya Haniva <diandaraa@gmail.com>
2022-06-26 23:02:03 +0700

    create fitur pembayaran user

--- pages/user/home/index.jsx
+++ pages/user/home/index.jsx
@@ -1,33 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import { FiUsers } from 'react-icons/fi';
-import { useRouter } from 'next/router';
-import Cookies from 'js-cookie';
-import axios from 'axios';
-import Carousel from '../../../components/afterlogin/carousel';
-import Card from '../../../components/afterlogin/home-card';
-import Layout from '../../../components/afterlogin/layout';
-import sessionUserLogin from '../../auth/sessionUserLogin';
+import React, { useState, useEffect } from "react";
+import { FiUsers, FiDollarSign } from "react-icons/fi";
+import { useRouter } from "next/router";
+import Cookies from "js-cookie";
+import axios from "axios";
+import Carousel from "../../../components/afterlogin/carousel";
+import Card from "../../../components/afterlogin/home-card";
+import Layout from "../../../components/afterlogin/layout";
+import sessionUserLogin from "../../auth/sessionUserLogin";
 
 function UserHomePage() {
   sessionUserLogin();
   const router = useRouter();
   const searchHandler = () => {
-    router.push('/user/search-guide');
+    router.push("/user/search-guide");
   };
 
   const [city, setCity] = useState(false);
 
   const getCity = async () => {
     const api = `${process.env.NEXT_PUBLIC_ENDPOINT}/cities/${Cookies.get(
-      'idCity',
+      "idCity"
     )}`;
 
     try {
       const response = await axios({
         url: api,
-        method: 'GET',
+        method: "GET",
         headers: {
-          authorization: `Bearer ${Cookies.get('pramunesiaAppToken')}`,
+          authorization: `Bearer ${Cookies.get("pramunesiaAppToken")}`,
         },
       });
 
@@ -44,26 +44,28 @@ function UserHomePage() {
   return (
     <Layout pageTitle="user-home">
       <h3 className="h3 my-3">
-        Rekomendasi Wisata di
-        {' '}
-        {city ? city.name : null}
-        {' '}
+        Rekomendasi Wisata di {city ? city.name : null}{" "}
       </h3>
       <Carousel />
       <div className="my-5">
-        <h3 className="h3 my-3">Pilih sesuai kebutuhanmu</h3>
-        <div className="row">
-          <div className="col-sm-12 col-md-6 col-lg-6 ps-0">
+        <h3 className="h3">Pilih sesuai kebutuhanmu</h3>
+        <div className="row my-3">
+          <div className="col-sm-12 col-md-6 col-lg-6  mb-4">
             <div onClick={searchHandler}>
               <Card
                 icons={<FiUsers size="4rem" color="white" />}
                 title="Cari Pemandu Wisata"
-                desc="Temukan pemandu wisata andalanmu
-                disini!"
+                desc="Temukan pemandu wisata andalanmu disini!"
               />
             </div>
           </div>
-          <div className="col-sm-12 col-md-6 col-lg-6" />
+          <div className="col-sm-12 col-md-6 col-lg-6  mb-4">
+            <Card
+              icons={<FiDollarSign size="4rem" color="white" />}
+              title="Pembayaran"
+              desc="Lakukan pembayaran disini!"
+            />
+          </div>
         </div>
       </div>
     </Layout>
