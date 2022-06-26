50747dc34f471bf8494befaae4e39a3c33452005
Diandara TResya Haniva <diandaraa@gmail.com>
2022-06-27 00:05:04 +0700

    update riwayat pemesanan dan etiket

--- pages/user/riwayat-pemesanan/tiket/index.jsx
+++ pages/user/riwayat-pemesanan/tiket/index.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import Image from 'next/image';
-import Brand from '../../../../public/nav-brand.png';
+import React from "react";
+import Image from "next/image";
+import Brand from "../../../../public/nav-brand.png";
 
 export default function Tiket() {
   return (
@@ -9,7 +9,7 @@ export default function Tiket() {
         <div className="d-flex justify-content-between mb-5">
           <div className="p-0">
             <h1 className="h5 text-orange">E-Ticket PRAMUNESIA</h1>
-            <p className="p">ID User : xxxxxxxx-xxxx-x</p>
+            <p className="p">Nomor Tiket: xxxxxxxx-xxxx-x</p>
           </div>
           <div className="p-0">
             <Image src={Brand} width={65} height={60} alt="navbar logo" />
@@ -18,7 +18,7 @@ export default function Tiket() {
         <h1 className="h4 mb-2">Nama Wisatawan</h1>
         <div className="row info-ticket py-3 mb-2">
           <div className="col-md-6 ps-0">
-            <p>Nama Pemandu Wisata :  Harry Potter</p>
+            <p>Nama Pemandu Wisata : Harry Potter</p>
             <p>Tanggal Pemesanan : 15-06-2022</p>
             <p>Tanggal Keberangkatan : 03-07-2022</p>
             <p>Tanggal Berakhir : 06-07-2022</p>
@@ -30,16 +30,29 @@ export default function Tiket() {
         <div className="note mb-4">
           <h1 className="h4 mb-2">Catatan:</h1>
           <ol>
-            <li>Tunjukkan e-ticket dan identitas diri ketika transaksi bersama pemandu wisata</li>
+            <li>
+              Tunjukkan e-ticket dan identitas diri ketika transaksi bersama
+              pemandu wisata
+            </li>
             <li>E-ticket bersifat sah dan dapat dipertanggung jawabkan </li>
           </ol>
         </div>
         <div className="d-flex align-items-center justify-content-end">
-          <small className="ms-auto">**Pembatalan transaksi setelah e-ticket dicetak, dilakukan berdasarkan persetujuan kedua belah pihak</small>
+          <small className="ms-auto">
+            **Pembatalan transaksi setelah e-ticket dicetak, dilakukan
+            berdasarkan persetujuan kedua belah pihak
+          </small>
         </div>
       </div>
       <div className="d-flex align-items-center justify-content-center">
-        <button type="button" id="printBtn" className="btn-orange fw-bold" onClick={() => window.print()}>Cetak Tiket</button>
+        <button
+          type="button"
+          id="printBtn"
+          className="btn-orange fw-bold"
+          onClick={() => window.print()}
+        >
+          Cetak Tiket
+        </button>
       </div>
     </div>
   );
