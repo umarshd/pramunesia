d0f080d51434a0f18533a57c52904a778ca516f5
Umar Sahid <44052666+umarshd@users.noreply.github.com>
2022-06-27 10:15:48 +0700

    Update index.jsx

--- components/afterlogin/carousel/index.jsx
+++ components/afterlogin/carousel/index.jsx
@@ -1,11 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { Zoom } from 'react-slideshow-image';
-import 'react-slideshow-image/dist/styles.css';
-import axios from 'axios';
-import Cookies from 'js-cookie';
+import React, { useState, useEffect } from "react";
+import { Zoom } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
+import axios from "axios";
+import Cookies from "js-cookie";
 
 export default function Carousel() {
   const [images, setImages] = useState(false);
+
   const propertiesImage = {
     duration: 5000,
     transitionDuration: 500,
@@ -15,38 +16,52 @@ export default function Carousel() {
     scale: 0.4,
   };
 
-  const getImages = async () => {
-    const api = `${process.env.NEXT_PUBLIC_ENDPOINT}/cities/${Cookies.get('idCity')}/images/${Cookies.get('imagename')}`;
+  const getDestination = async () => {
+    const api = `${process.env.NEXT_PUBLIC_ENDPOINT}/cities/${Cookies.get(
+      "idCity"
+    )}/destinations`;
 
     try {
       const response = await axios({
-        method: 'GET',
+        method: "GET",
         url: api,
         headers: {
-          authorization: `Bearer ${Cookies.get('pramunesiaAppToken')}`,
+          authorization: `Bearer ${Cookies.get("pramunesiaAppToken")}`,
         },
       });
       await setImages(response.data);
-      console.log(response.data);
     } catch (error) {
       console.log(error);
     }
   };
 
+  console.log(images);
   useEffect(() => {
-    getImages();
+    getDestination();
   }, []);
 
   return (
     <div className="slide-container">
       <Zoom {...propertiesImage}>
-        {images ? images.map((image, index) => (
-          <div key={index}>
-            <img src={image} alt="slide" className="rounded image-slide" />
-          </div>
-        )) : <div>Loading...</div>}
+        {images ? (
+          images.map((image, index) => (
+            <div key={index}>
+              <img
+                src={`${process.env.NEXT_PUBLIC_ENDPOINT}/cities/${Cookies.get(
+                  "idCity"
+                )}/destinations/image/${image.image}`}
+                alt="slide"
+                className="rounded image-slide"
+              />
+              <div>{`${process.env.NEXT_PUBLIC_ENDPOINT}/cities/${Cookies.get(
+                "idCity"
+              )}/destinations/image/${image.image}`}</div>
+            </div>
+          ))
+        ) : (
+          <div>Loading...</div>
+        )}
       </Zoom>
-
     </div>
   );
 }
